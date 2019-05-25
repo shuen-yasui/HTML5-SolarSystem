8018376f89765a3573e3862ceba50b8a6e9334c5
Shuen Yasui <shuenyasui@gmail.com>
2019-05-24 20:18:55 -0700

    Add orbit animation based on real planet data, add dotted orbit path

--- SolarSystem.js
+++ SolarSystem.js
@@ -1,5 +1,5 @@
 class Particle {
-	constructor(name,path,r){
+	constructor(name,path,r,vel){
 		this.centerX = window.innerWidth/2;
 		this.centerY = window.innerHeight/2;
 		this.x;
@@ -7,23 +7,40 @@ class Particle {
 		this.couple;
 		this.radius = r;
     this.pathRadius = path;
-		this.vel = 0.01;
+		this.vel = 0.01 / vel;
     this.radians = 0;
 		this.alpha = 1;
 	}
 	update(){
-    // Loc
-    // Update Positions
+		// If sun draw
+		if (this.pathRadius == 0){
+			this.x = this.centerX;
+			this.y = this.centerY;
+			this.drawPlanet();
+			return
+		}
+		// Update Positions
 		this.radians += this.vel;
 		this.x = this.centerX + (this.pathRadius * (Math.cos(this.radians)));
 		this.y = this.centerY + (this.pathRadius * (Math.sin(this.radians)) * .2);
-    // Path
-    c.beginPath();
-    c.arc(this.centerX,this.centerY,this.pathRadius,0,Math.PI * 2,false);
-    c.strokeStyle = "white";
-    c.stroke();
-    c.closePath();
-    // Planet
+    // Draw Path
+    var pRadi = 0
+    var pX = 0
+    var pY = 0
+		var orbit = 64*(this.pathRadius/500);
+    while (pRadi < Math.PI * 2){
+			c.beginPath();
+      pX = this.centerX + (this.pathRadius * (Math.cos(pRadi)));
+  		pY = this.centerY + (this.pathRadius * (Math.sin(pRadi)) * .2)
+      c.arc(pX,pY,0.5,0,Math.PI * 2,false);
+      c.strokeStyle = 'rgb(50,50,50)';
+      c.stroke();
+      pRadi += (Math.PI/(16+orbit));
+			c.closePath();
+    }
+    this.drawPlanet();
+	}
+	drawPlanet(){
     c.beginPath();
     c.arc(this.x,this.y,this.radius,0,Math.PI * 2,false);
     c.strokeStyle = "rgba(255,255,255,"+this.alpha+")";
@@ -42,16 +59,16 @@ var particles = [];
 init();
 
 function init(){
-  particles.push(new Particle("Sun",0,20));
-  particles.push(new Particle("Mercury",100,10));
-  particles.push(new Particle("Venus",150,10));
-  particles.push(new Particle("Earth",200,10));
-  particles.push(new Particle("Mars",250,10));
-  particles.push(new Particle("Jupiter",300,10));
-  particles.push(new Particle("Saturn",350,10));
-  particles.push(new Particle("Uranus",400,10));
-  particles.push(new Particle("Neptune",450,10));
-  particles.push(new Particle("Pluto",500,10));
+  particles.push(new Particle("Sun",0,20,0));
+  particles.push(new Particle("Mercury",100,10,0.2));
+  particles.push(new Particle("Venus",150,10,0.6));
+  particles.push(new Particle("Earth",200,10,1));
+  particles.push(new Particle("Mars",250,10,1.9));
+  particles.push(new Particle("Jupiter",300,10,11.9));
+  particles.push(new Particle("Saturn",350,10,29.5));
+  particles.push(new Particle("Uranus",400,10,84));
+  particles.push(new Particle("Neptune",450,10,164.8));
+  particles.push(new Particle("Pluto",500,10,248));
 	update();
 }
 function update(){
